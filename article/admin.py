c427085419be43f739b81bd09438c8e4916a4810
ParveenMalethia <prince.malethia.36@gmail.com>
2021-04-27 05:14:56 +0530

    Models Registered in Admin

--- article/admin.py
+++ article/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .admin import (Article,ArticleComment,ArticleVote)
 # Register your models here.
+
+admin.site.register(Article)
+admin.site.register(ArticleComment)
+admin.site.register(ArticleVote)

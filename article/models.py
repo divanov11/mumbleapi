75ae2ab70fc5d6ce3e55480551946c0f8aa4e7cb
ParveenMalethia <prince.malethia.36@gmail.com>
2021-04-26 23:44:21 +0530

    article app added

---
+++ article/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from django.contrib.auth.models import User
+import uuid
+# Create your models here.
+
+class Article(models.Model):
+    user = models.ForeignKey(User,on_delete=models.SET_NULL, null=True, blank=True)
+    content = models.TextField(max_length=10000)
+    tags = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.user.username)
+
+
+class ArticleComment(models.Model):
+    article = models.ForeignKey(Article,on_delete=models.CASCADE)
+    user = models.ForeignKey(User,on_delete=models.SET_NULL, null=True, blank=True)
+    content = models.TextField(max_length=1000)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.user.username)
+
+
+class ArticleVote(models.Model):
+    user = models.ForeignKey(User,on_delete=models.SET_NULL, null=True, blank=True)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    comment = models.ForeignKey(ArticleComment, on_delete=models.CASCADE)
+    value = models.BigIntegerField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.comment} - count - {self.value}"

d5754d7ff1e6ede6e93d27961b0e618cfde928a1
ParveenMalethia <prince.malethia.36@gmail.com>
2021-04-30 03:29:30 +0530

    Heroku Database Setting Added

--- mumblebackend/settings.py
+++ mumblebackend/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 from datetime import timedelta
+import django_heroku
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -143,6 +144,9 @@ DATABASES = {
     }
 }
 
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators

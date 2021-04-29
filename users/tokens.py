cc2691fb8d77bdbcf51c4206e09c785cd415942d
ParveenMalethia <prince.malethia.36@gmail.com>
2021-04-29 19:08:19 +0530

    tokens files deleted

--- users/tokens.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.tokens import PasswordResetTokenGenerator  
-import six
-class AccountActivationTokenGenerator(PasswordResetTokenGenerator):  
-        def _make_hash_value(self, user, timestamp):  
-            return (  
-                six.text_type(user.pk) + six.text_type(timestamp) +  
-                six.text_type(user.is_active)  
-            )
-account_activation_token = AccountActivationTokenGenerator()
--- users/views.py
+++ users/views.py
@@ -15,7 +15,6 @@ from django.core.mail import EmailMessage
 from django.template.loader import render_to_string
 from django.utils.encoding import force_bytes
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
-from .tokens import account_activation_token
 
 from .models import UserProfile
 from .serializers import UserProfileSerializer

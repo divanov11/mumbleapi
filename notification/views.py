b88bba60c057000b60682b7b80a4b4f28543aed5
Cody Seibert <cseibert@flexion.us>
2021-04-30 18:39:44 -0400

    adding auth checks

--- notification/views.py
+++ notification/views.py
@@ -1,21 +1,26 @@
 from django.shortcuts import render
 from rest_framework.response import Response 
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 from django.db.models import Q
 from .models import Notification
 from .serializers import NotificationSerializer
 
 @api_view(['GET'])
+@permission_classes((IsAuthenticated,))
 def getNotification(request, pk):
     try:
         notification = Notification.objects.get(id=pk)
+        if notification.to_user != request.user:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
         serializer = NotificationSerializer(notification, many=False)
         return Response(serializer.data)
     except Exception as e:
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 @api_view(['POST'])
+@permission_classes((IsAuthenticated,))
 def readNotification(request,pk):
     try:
         notification= Notification.objects.get(id=pk)

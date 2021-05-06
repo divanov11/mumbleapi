3c1aeb0b4e87535675dfe6b4d7b7aff0905f7c63
divanov11 <ivanovsin11@gmail.com>
2021-05-06 13:23:54 -0500

    Added temp profile pics for active users

--- mumblebackend/settings.py
+++ mumblebackend/settings.py
@@ -162,6 +162,7 @@ if os.getcwd() == '/app':
     #SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
     #SECURE_SSL_REDIRECT = True
     #DEBUG = True
+
     
     DATABASES = {
         'default': {

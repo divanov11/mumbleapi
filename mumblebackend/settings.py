57df01ea0dcb084ac1f49797680ad00c99c46cce
ParveenMalethia <prince.malethia.36@gmail.com>
2021-05-06 21:36:16 +0530

    sentry integrated

--- mumblebackend/settings.py
+++ mumblebackend/settings.py
@@ -15,6 +15,24 @@ import os
 from datetime import timedelta
 import django_heroku
 
+# sentry configs
+
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+sentry_sdk.init(
+    dsn="https://de808f6f605c4fd79120ddb21f073904@o599875.ingest.sentry.io/5743882",
+    integrations=[DjangoIntegration()],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent

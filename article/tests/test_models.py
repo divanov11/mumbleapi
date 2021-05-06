a330d434cb0976af867f5cafc4796d72ca00fd31
ParveenMalethia <prince.malethia.36@gmail.com>
2021-05-07 00:41:11 +0530

    test for urls

--- article/tests/test_models.py
+++ article/tests/test_models.py
@@ -1,7 +1,18 @@
 from django.test import TestCase
 from django.test import SimpleTestCase
+from django.urls import reverse , resolve
+from article.views import *
 
 class TestUrls(SimpleTestCase):
     
-    def test_list_article_url_is_resolved(self):
-        pass
+    def test_articles_url_is_resolved(self):
+        url = reverse('articles')
+        self.assertEquals(resolve(url).func,articles)
+
+    def test_articles_created_url_is_resolved(self):
+        url = reverse('create-article')
+        self.assertEquals(resolve(url).func,createArticle)
+
+    def test_articles_vote_url_is_resolved(self):
+        url = reverse('vote')
+        self.assertEquals(resolve(url).func,updateVote)

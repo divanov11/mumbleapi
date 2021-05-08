f7bdb158d815fe92d7b9d2d0bb0ef0e6b75bcc8d
ParveenMalethia <prince.malethia.36@gmail.com>
2021-05-08 18:39:11 +0530

    API testing Setup

--- users/tests.py
+++ users/tests.py
@@ -1,5 +1,10 @@
-from django.test import TestCase
+from django.test import RequestFactory,TestCase
 from django.contrib.auth.models import User
+from rest_framework.test import APIClient
+from rest_framework.authtoken.models import Token
+from django.urls import include, path, reverse
+from rest_framework import status
+from rest_framework.test import APITestCase, URLPatternsTestCase
 # Create your tests here.
 
 class UserTest(TestCase):
@@ -19,3 +24,12 @@ class UserTest(TestCase):
 
     def test_user_password(self):
         self.assertTrue(self.user.check_password(self.user_password))
+
+class UsersTestAPI(APITestCase):
+
+    def test_login_via_api(self):
+        token = Token.objects.get(user__username='test')
+        client = APIClient()
+        client.login(username='test', password='test@123')
+        client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
+        client.login()

47eef2bbd3aed6f4effcc5bc4659d8e36b9224bf
ParveenMalethia <prince.malethia.36@gmail.com>
2021-05-08 18:49:46 +0530

    Better API testing

--- users/tests.py
+++ users/tests.py
@@ -7,29 +7,15 @@ from rest_framework import status
 from rest_framework.test import APITestCase, URLPatternsTestCase
 # Create your tests here.
 
-class UserTest(TestCase):
-    
-    def setUp(self):
-        user = User(username='Praveen',email='prince.malethia.36@gmail.com')
-        user.is_staff = True
-        user.is_superuser = True
-        self.user_password = 'S0MtaingWiredPhassWarld'
-        user.set_password(self.user_password)
-        user.save()
-        self.user = user
-
-    def test_user_exists(self):
-        total_user = User.objects.all().count()
-        self.assertEquals(total_user,1)
-
-    def test_user_password(self):
-        self.assertTrue(self.user.check_password(self.user_password))
-
-class UsersTestAPI(APITestCase):
-
-    def test_login_via_api(self):
-        token = Token.objects.get(user__username='test')
-        client = APIClient()
-        client.login(username='test', password='test@123')
-        client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
-        client.login()
+class AccountTests(APITestCase):
+    def test_create_account(self):
+        url = reverse('users-api:register')
+        data = {
+            'username':'test',
+            'email':'test@gmail.com', 
+            'password':'test@123'
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(User.objects.count(), 1)
+        self.assertEqual(User.objects.get().username, 'test')

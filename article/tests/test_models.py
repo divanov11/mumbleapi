ac2aa37ce74f60f7a6c6d15050f208a487724d50
Praveen Malethia <59171685+PraveenMalethia@users.noreply.github.com>
2021-05-09 16:12:48 +0530

    Removed unused imports ! (#76)
    * Made Article create test
    
    * deleted user API test
    
    * removed imports

--- article/tests/test_models.py
+++ article/tests/test_models.py
@@ -1,11 +1,35 @@
-from rest_framework.authtoken.models import Token
+from django.test import RequestFactory,TestCase
+from django.contrib.auth.models import User
 from rest_framework.test import APIClient
-from django.test import TestCase
+from django.urls import include, path, reverse , resolve
+from rest_framework import status
+from rest_framework.test import APITestCase, URLPatternsTestCase
+from article.views import createArticle
 
-class ArticleTestCases(TestCase):
-    
+class ArticleTestCases(APITestCase):
+
     def setUp(self):
-        token = Token.objects.get(user__username='test')
+        url = reverse('users-api:register')
+        data = {
+            'username':'test',
+            'email':'test@gmail.com', 
+            'password':'test@123'
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(User.objects.count(), 1)
+        self.assertEqual(User.objects.get().username, 'test')
+
+    def test_create_article(self):
+        url = reverse('create-article')
+        user = User.objects.get(username='test')
         client = APIClient()
-        client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
         client.force_authenticate(user=user)
+        data = {
+            'title':"Title of Article",
+            'content':"Content for article",
+            'tags':"Tags for article"
+        }
+        response = client.post(url,data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(resolve(url).func,createArticle)
--- users/tests.py
+++ users/tests.py
@@ -1,14 +1,14 @@
-from django.test import RequestFactory,TestCase
 from django.contrib.auth.models import User
 from rest_framework.test import APIClient
-from rest_framework.authtoken.models import Token
-from django.urls import include, path, reverse
+from django.urls import include, path, reverse , resolve
 from rest_framework import status
-from rest_framework.test import APITestCase, URLPatternsTestCase
+from rest_framework.test import APITestCase
+from . import views
 # Create your tests here.
 
 class AccountTests(APITestCase):
-    def test_create_account(self):
+
+    def setUp(self):
         url = reverse('users-api:register')
         data = {
             'username':'test',

4ce01c7ed47200bc3fec582488246244b38fc5fb
ParveenMalethia <prince.malethia.36@gmail.com>
2021-05-11 23:16:20 +0530

    :arrow_upper_right: auth classes added

--- article/views.py
+++ article/views.py
@@ -18,6 +18,7 @@ def getArticle(request, pk):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 @api_view(['PUT'])
+@permission_classes((IsAuthenticated,))
 def editArticle(request,pk):
     try:
         article = Article.objects.get(id=pk)
@@ -35,6 +36,7 @@ def editArticle(request,pk):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 @api_view(['DELETE'])
+@permission_classes((IsAuthenticated,))
 def deleteArticle(request,pk):
     try:
         article = Article.objects.get(id=pk)
@@ -47,6 +49,7 @@ def deleteArticle(request,pk):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 @api_view(['GET'])
+@permission_classes((IsAuthenticated,))
 def articles(request):
     query = request.query_params.get('q')
     if query == None:
@@ -60,6 +63,7 @@ def articles(request):
 
 
 @api_view(['PUT'])
+@permission_classes((IsAuthenticated,))
 def editArticleComment(request,pk):
     try:
         comment = ArticleComment.objects.get(id=pk)
@@ -72,6 +76,7 @@ def editArticleComment(request,pk):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 @api_view(['DELETE'])
+@permission_classes((IsAuthenticated,))
 def deleteArticleComment(request,pk):
     try:
         comment = ArticleComment.objects.get(id=pk)

ddf29cb29167706d2334f1ef79af16dd22278631
Gautham P Krishnan <gauthampkrishnan@ieee.org>
2021-05-13 07:53:02 +0530

    SSL redirect issue solved

--- mumblebackend/settings.py
+++ mumblebackend/settings.py
@@ -161,9 +161,9 @@ WSGI_APPLICATION = 'mumblebackend.wsgi.application'
 
 
 if os.getcwd() == '/app':
-    #SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-    #SECURE_SSL_REDIRECT = True
-    #DEBUG = True
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    SECURE_SSL_REDIRECT = True
+    DEBUG = False
 
     
     DATABASES = {

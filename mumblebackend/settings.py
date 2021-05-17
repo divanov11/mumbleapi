8d60ff5a0f70c9f9ec1c9ccf745f45d1ba9916fd
Cody Seibert <cseibert113@gmail.com>
2021-05-16 23:11:00 -0400

    setting the jwt secret key from os env

--- mumblebackend/settings.py
+++ mumblebackend/settings.py
@@ -44,7 +44,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = get_random_secret_key()
+SECRET_KEY = os.environ.get('SECRET_KEY') or 'my secret key for local testing'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

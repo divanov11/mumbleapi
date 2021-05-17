15104990da48ff4614736936837df8b98f7768df
Abhi Vempati <abhijitvempati@gmail.com>
2021-05-17 14:30:08 -0400

    Admin panel testing

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import UserProfile
+from .models import UserProfile, TopicTag
 
 admin.site.register(UserProfile)
+admin.site.register(TopicTag)

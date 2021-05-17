694e9ba33f0138059af2be772dd397e2699d84b8
Abhi Vempati <abhijitvempati@gmail.com>
2021-05-17 14:44:00 -0400

    Skills admin testing completed

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import UserProfile, TopicTag
+from .models import TopicTag, SkillTag, UserProfile
 
-admin.site.register(UserProfile)
 admin.site.register(TopicTag)
+admin.site.register(SkillTag)
+admin.site.register(UserProfile)

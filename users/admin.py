9bc4eca735ad3aa19ed9917c3e77801aac0203d2
Abhi Vempati <35072868+abhivemp@users.noreply.github.com>
2021-05-19 01:08:32 -0400

    Skills and Tags models implementation (#134)
    * Tags field with manytomany relationship
    
    * Admin panel testing
    
    * Skills with manytomany added
    
    * Skills admin testing completed
    
    * formats and comments

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import UserProfile
+from .models import TopicTag, SkillTag, UserProfile
 
+admin.site.register(TopicTag)
+admin.site.register(SkillTag)
 admin.site.register(UserProfile)
--- users/models.py
+++ users/models.py
@@ -1,9 +1,25 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
+from django.db.models.fields import related
 import uuid
 
 
-# Create your models here.
+
+# A topic tag is added to by the user so they can content on their feed with the 
+# related tags that
+# They have selected
+class TopicTag(models.Model):
+    name = models.CharField(primary_key=True, max_length=150, null=False, blank=False)
+
+    def __str__(self):
+        return self.name
+
+# Skills are added by teh user to indicate topics they are proficient in
+class SkillTag(models.Model):
+    name = models.CharField(primary_key=True, max_length=150, null=False, blank=False)
+
+    def __str__(self):
+        return self.name
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -13,8 +29,8 @@ class UserProfile(models.Model):
     bio = models.TextField(null=True)
     vote_ratio = models.IntegerField(blank=True, null=True, default=0)
     followers_count = models.IntegerField(blank=True, null=True, default=0)
-    #skills = 
-    #interests = 
+    skills = models.ManyToManyField(SkillTag, related_name='personal_skills', blank=True)
+    interests = models.ManyToManyField(TopicTag, related_name='topic_interests', blank=True)
     followers = models.ManyToManyField(User, related_name='following', blank=True)
     email_verified = models.BooleanField(default=False)
     id = models.UUIDField(default=uuid.uuid4,  unique=True, primary_key=True, editable=False)

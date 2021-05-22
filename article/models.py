1197ec6ebee1e54fa7c3dd2fac66feb8e0e09a30
itzomen <peng.akebuon2468@gmail.com>
2021-05-22 14:38:18 +0100

    added article tags field

--- article/models.py
+++ article/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from ckeditor.fields import RichTextField
+from users.models import TopicTag
 import uuid
 # Create your models here.
 
@@ -9,7 +10,8 @@ class Article(models.Model):
     user = models.ForeignKey(User,on_delete=models.SET_NULL, null=True, blank=True)
     title = models.CharField(max_length=500, default="untitled")
     content = RichTextField(max_length=10000)
-    tags = models.CharField(max_length=100)
+    # discussion tags from user model
+    tags = models.ManyToManyField(TopicTag, related_name='article_tags', blank=True) 
     created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):

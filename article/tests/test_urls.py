6cc075046eb69e66c0a294a312b95ad373fd311f
Cody Seibert <cseibert113@gmail.com>
2021-05-23 19:25:41 -0400

    fixing article tests due to missing app name in reverse call

--- article/tests/test_urls.py
+++ article/tests/test_urls.py
@@ -6,33 +6,33 @@ from article.views import *
 class TestUrls(SimpleTestCase):
     
     def test_articles_url_is_resolved(self):
-        url = reverse('articles')
+        url = reverse('mumbles-api-articles:articles')
         self.assertEquals(resolve(url).func,articles)
 
     def test_articles_created_url_is_resolved(self):
-        url = reverse('create-article')
+        url = reverse('mumbles-api-articles:create-article')
         self.assertEquals(resolve(url).func,createArticle)
 
     def test_articles_vote_url_is_resolved(self):
-        url = reverse('article-vote')
+        url = reverse('mumbles-api-articles:article-vote')
         self.assertEquals(resolve(url).func,updateVote)
 
     def test_get_article_url_is_resolved(self):
-        url = reverse('get-article',args=['sOmE-iD'])
+        url = reverse('mumbles-api-articles:get-article',args=['sOmE-iD'])
         self.assertEquals(resolve(url).func,getArticle)
 
     def test_edit_article_url_is_resolved(self):
-        url = reverse('edit-article',args=['sOmE-iD'])
+        url = reverse('mumbles-api-articles:edit-article',args=['sOmE-iD'])
         self.assertEquals(resolve(url).func,editArticle)
 
     def test_delete_article_url_is_resolved(self):
-        url = reverse('delete-article',args=['sOmE-iD'])
+        url = reverse('mumbles-api-articles:delete-article',args=['sOmE-iD'])
         self.assertEquals(resolve(url).func,deleteArticle)
 
     def test_edit_article_comment_url_is_resolved(self):
-        url = reverse('edit-article-comment',args=['sOmE-iD'])
+        url = reverse('mumbles-api-articles:edit-article-comment',args=['sOmE-iD'])
         self.assertEquals(resolve(url).func,editArticleComment)
 
     def test_delete_article_comment_url_is_resolved(self):
-        url = reverse('delete-article-comment',args=['sOmE-iD'])
+        url = reverse('mumbles-api-articles:delete-article-comment',args=['sOmE-iD'])
         self.assertEquals(resolve(url).func,deleteArticleComment)

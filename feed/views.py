048700571883d4caf0a565fe30b0fafa067e9bee
divanov11 <ivanovsin11@gmail.com>
2021-06-07 14:22:58 -0400

    Added most recent mumbles

--- feed/views.py
+++ feed/views.py
@@ -34,8 +34,15 @@ def mumbles(request):
     mumbles = list(Mumble.objects.filter(parent=None, user__id__in=ids).order_by("-created"))[0:5]
     #mumbles = list(mumbles.filter(Q(user__userprofile__name__icontains=query) | Q(content__icontains=query)))
 
+    recentMumbles = Mumble.objects.filter(Q(parent=None)).order_by("-created")[0:10]
+
     #Query top ranked mumbles and attach to end of original queryset
     topMumbles = Mumble.objects.filter(Q(parent=None)).order_by("-vote_rank", "-created")
+
+    #Add top ranked mumbles to feed after prioritizing follow list 
+    for mumble in recentMumbles:
+        if mumble not in mumbles:
+            mumbles.append(mumble)
 
     #Add top ranked mumbles to feed after prioritizing follow list 
     for mumble in topMumbles:

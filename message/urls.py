101825d8aecba7eac4e31046e7b4b15b36c55f77
Cody Seibert <cseibert113@gmail.com>
2021-06-13 22:30:43 -0400

    adding read message filter

--- message/urls.py
+++ message/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('<str:pk>/read/',views.read_message,name='read-notification'),
     path('', views.get_messages, name="get-messages"),
+    path('<str:pk>/read/', views.read_message, name="read-message"),
+    path('unread/count/', views.get_unread_messages_count, name="get-unread-messages-count"),
     path('create/', views.create_message, name="create-message"),
 ]
--- message/views.py
+++ message/views.py
@@ -2,29 +2,40 @@ from django.contrib.auth.models import User
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework import status
 
 from .serializers import MessageSerializer
 from .models import Message
 
 
-# @api_view(['PUT'])
-# @permission_classes((IsAuthenticated,))
-# def read_notification(request, pk):
-#     try:
-#         notification = Notification.objects.get(id=pk)
-#         if notification.to_user == request.user:
-#             notification.delete()
-#             return Response(status=status.HTTP_204_NO_CONTENT)
-#         else:
-#             return Response(status=status.HTTP_401_UNAUTHORIZED)
-#     except Exception as e:
-#         return Response({'details': f"{e}"},status=status.HTTP_204_NO_CONTENT)
+@api_view(['PUT'])
+@permission_classes((IsAuthenticated,))
+def read_message(request, pk):
+    try:
+        message = Message.objects.get(id=pk)
+        if message.to_user == request.user:
+            message.is_read = True
+            message.save()
+            serializer = MessageSerializer(message, many=False)
+            return Response(serializer.data)
+        else:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+    except Exception as e:
+        return Response({'details': f"{e}"},status=status.HTTP_204_NO_CONTENT)
+
+@api_view(['GET'])
+@permission_classes((IsAuthenticated,))
+def get_unread_messages_count(request):
+    count = request.user.messages.filter(is_read=False).count()
+    return Response({
+        'count': count
+    })
 
 
 @api_view(['GET'])
 @permission_classes((IsAuthenticated,))
 def get_messages(request):
-    messages = request.user.messages.all().order_by('-created')
+    messages = request.user.messages.order_by('-created')
     serializer = MessageSerializer(messages, many=True)
     return Response(serializer.data)
 
@@ -34,7 +45,6 @@ def create_message(request):
     user = request.user
     data = request.data
     to_user = User.objects.get(id=data['to_user'])
-
     message = Message.objects.create(
         to_user=to_user,
         created_by=user,

ba38bd1ba63e7ae652b73551a30c9aee384b0e45
azeezdot123 <headofstate123@gmail.com>
2021-07-26 04:16:50 +0100

    Refactored the  models to follow DRY principle

--- discussion/models.py
+++ discussion/models.py
@@ -5,37 +5,36 @@ from users.models import TopicTag
 import uuid
 
 
-class Discussion(models.Model):
+class BaseModel(models.Model):
     id = models.UUIDField(default=uuid.uuid4,  unique=True, primary_key=True, editable=False)
-    user = models.ForeignKey(User,on_delete=models.SET_NULL, null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    class Meta:
+        abstract = True
+
+
+class Discussion(BaseModel):
     headline = models.CharField(max_length=500, default="no headline")
     content = RichTextField(max_length=10000)
     # discussion tags from user model
-    tags = models.ManyToManyField(TopicTag, related_name='discussion_tags', blank=True) 
-    created = models.DateTimeField(auto_now_add=True)
+    tags = models.ManyToManyField(TopicTag, related_name='discussion_tags', blank=True)
 
     def __str__(self):
         return str(self.headline)
 
 
-class DiscussionComment(models.Model):
-    id = models.UUIDField(default=uuid.uuid4,  unique=True, primary_key=True, editable=False)
-    discussion = models.ForeignKey(Discussion,on_delete=models.CASCADE)
-    user = models.ForeignKey(User,on_delete=models.SET_NULL, null=True, blank=True)
+class DiscussionComment(BaseModel):
+    discussion = models.ForeignKey(Discussion, on_delete=models.CASCADE)
     content = models.TextField(max_length=1000)
-    created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return str(self.user.username)
 
 
-class DiscussionVote(models.Model):
-    id = models.UUIDField(default=uuid.uuid4,  unique=True, primary_key=True, editable=False)
-    user = models.ForeignKey(User,on_delete=models.SET_NULL, null=True, blank=True)
+class DiscussionVote(BaseModel):
     discussion = models.ForeignKey(Discussion, on_delete=models.CASCADE)
-    comment = models.ForeignKey(DiscussionComment, on_delete=models.SET_NULL,null=True, blank=True)
+    comment = models.ForeignKey(DiscussionComment, on_delete=models.SET_NULL, null=True, blank=True)
     value = models.IntegerField(blank=True, null=True, default=0)
-    created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.discussion} - count - {self.value}"
--- message/models.py
+++ message/models.py
@@ -2,25 +2,28 @@ from django.db import models
 from users.models import UserProfile
 import uuid
 
-class Thread(models.Model):
+
+class BaseModel(models.Model):
     id = models.UUIDField(default=uuid.uuid4,  unique=True, primary_key=True, editable=False)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        abstract = True
+class Thread(BaseModel):
     sender = models.ForeignKey(UserProfile,on_delete=models.SET_NULL,null=True,related_name="sender")
     reciever = models.ForeignKey(UserProfile,on_delete=models.SET_NULL,null=True,related_name="reciever")
     updated = models.DateTimeField(auto_now=True)
-    timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return str(self.sender.username + " and " + self.reciever.username)
 
 
-class UserMessage(models.Model):
-    id = models.UUIDField(default=uuid.uuid4,  unique=True, primary_key=True, editable=False)
+class UserMessage(BaseModel):
     thread = models.ForeignKey(
         Thread, on_delete=models.CASCADE,related_name="messages")
     sender = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     body = models.TextField(null=True,blank=True)
     is_read = models.BooleanField(default=False)
-    timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return str(self.body)
